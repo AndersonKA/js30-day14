48601c2673e7d83d721eaa51eb94b9789d63e6f5
Kae Anderson <andersonkaem@gmail.com>
2017-06-02 21:26:09 -0500

    JS30 Day 14

---
+++ index.js
@@ -0,0 +1,71 @@
+// start with strings, numbers and booleans
+  // let age = 100;
+  // let age2 = age;
+  // console.log(age, age2);
+  // age = 200;
+  // console.log(age, age2);
+  //
+  // let name = 'doctor';
+  // let name2 = name;
+  // console.log(name, name2);
+  // name = 'thedoctor';
+  // console.log(name, name2);
+
+// Let's say we have an array
+const players = ['Doctor', 'Nyssa', 'Tegan', 'Adric'];
+
+// and we want to make a copy of it.
+const team = players;
+console.log(players, team);
+
+// You might think we can just do something like this:
+// team[3] = 'K9';
+
+// but if we update that array it references the first array
+
+// So, how do we fix this? We take a copy instead!
+const team2 = players.slice();
+
+// create a new array and concat the old one in
+const team3 = [].concat(players);
+
+// or use the new ES6 Spread
+const team4 = [...players];
+const team5 = Array.from(players);
+
+// now when we update it, the original one isn't changed
+// The same thing goes for objects, let's say we have a person object
+
+// with Objects
+const person = {
+  name: 'The Doctor',
+  age: 1700
+};
+
+// and think we make a copy:
+// const captain = person;
+// captain.number = 99;
+
+// how do we take a copy instead?
+const cap2 = Object.assign({}, person, { number: 99, age: 2000 });
+console.log(cap2);
+
+// We will hopefully soon see the object ...spread
+// const cap3 = {...person};
+
+// Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+const doctor = {
+  name: 'Doctor',
+  age: 1700,
+  social: {
+    twitter: '@TheDoctor',
+    facebook: '@JohnSmith'
+  }
+};
+
+console.clear();
+console.log(doctor);
+
+// const doctor2 = object.assign({}, doctors);
+//
+// const dev2 = JSON.parson(JSON.stringify(doctor));
